2c41b3e7a9a4a253fb5a2d2039aa6959487d25b4
jackietng <jn1thythy56@gmail.com>
2024-11-27 23:34:04 -0700

    added onClick behavior to cards title on dashboard

--- client/src/components/Dashboard/TopContainer.tsx
+++ client/src/components/Dashboard/TopContainer.tsx
@@ -1,20 +1,28 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import { GiCardPlay } from "react-icons/gi";
 import styles from './Dashboard.module.scss'; 
-import CardGallery from './CardGallery';
 
 function TopContainer() { 
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        console.log('Navigating to the Cards Page');
+        navigate('/cards')
+    }
 
 return (
     <div>
         <div className={styles.topContainer}>
-
             <div className={styles.title}>
                 <i className={styles.icon} >
                     <GiCardPlay/>
-                </i>    
-                <h2>Cards</h2>
+                </i>  
 
+                  {/* Add onClick handler to Cards */}
+                <h2 onClick={handleClick} className={styles.clickableTitle}>Cards</h2>
+
+                {/* Buttons for filtering/sorting */}
                 <button className={styles.button}>A to Z</button>
                 <button className={styles.button}>Mana</button>
                 <button className={styles.button}>Cost</button>
