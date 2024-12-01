7ae38afb055622c8024a6c3c4986c74159bcb468
flynno <luke.laz.flynn@gmail.com>
2024-12-01 15:09:57 -0700

    added basic client route handlers

--- client/src/api/mtgAPI.tsx
+++ client/src/api/mtgAPI.tsx
@@ -1,2 +1,63 @@
 // src/api/mtgAPI.tsx
 
+type mana = "W" | "U" | "B" | "R" | "G" | "C"  // mana types - white, blue, black, red, green, colorless
+interface filters {
+  name?: string
+  type_line?: string
+  oracle_text?: string
+  subtype?: string
+  colors?: mana[]
+  cmc?: number
+  power?: number
+  toughness?: number
+  page?: number
+  limit?: number
+}
+
+const getAllCards = async (filters: filters) => {
+  const response = await fetch('/api/cards', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(filters),
+  });
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  const data = await response.json();
+  return data;
+}
+
+const getNextPage = async (url: string) => {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  const data = await response.json();
+  return data;
+}
+
+// const login = async (userInfo: UserLogin) => {
+//   const response = await fetch('/auth/login', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(userInfo),
+//   });
+  
+//   if (!response.ok) {
+//     throw new Error('Network response was not ok');
+//   }
+
+//   const data: LoginResponse = await response.json();
+//   console.log(data);
+//   return data;
+// }
+
+export { getAllCards, getNextPage }
