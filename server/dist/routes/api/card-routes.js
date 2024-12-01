d68da86610a2b6e5f744763ae859503bd819f27f
flynno <luke.laz.flynn@gmail.com>
2024-12-01 15:11:53 -0700

    removed dist files from repo

--- server/dist/routes/api/card-routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAllCards, getCardById } from "../../controllers/card-controllers.js";
-const router = Router();
-// GET / - Get all cards using query parameters and pagination
-router.get("/", getAllCards);
-// GET /:id - Get card by ID
-router.get("/:id", getCardById);
-export { router as cardRouter };
