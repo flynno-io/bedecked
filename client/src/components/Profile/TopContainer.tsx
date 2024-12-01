4ef98a02a368a92cdb62e49eab85a392c70304ba
Duck Method <129229008+Duck-Method@users.noreply.github.com>
2024-11-30 17:25:44 -0700

    boilerplate added to profile components and page

---
+++ client/src/components/Profile/TopContainer.tsx
@@ -0,0 +1,14 @@
+import ProfileDetails from "./ProfileDetails";
+
+function TopContainer() {
+    return(
+        <div>
+            <p>
+                Top Container
+            </p>
+            <ProfileDetails/>
+        </div>
+    )
+}
+
+export default TopContainer;
