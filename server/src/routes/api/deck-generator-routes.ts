797bb94170318814abd9c6ec7a148446576c130a
flynno <luke.laz.flynn@gmail.com>
2024-12-02 20:30:00 -0700

    updated Deck Generator route

--- server/src/routes/api/deck-generator-routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-import { generateDeck } from "../../controllers/deck-generator-controllers.js"
-
-const router = Router()
-
-// *** Root route is /api/generate-deck ***
-
-// Generate a deck
-router.post("/generate-deck", generateDeck)
